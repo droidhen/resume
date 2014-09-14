06c984a8317c5313e3282d2e08116409b5d14fa0
luyao <lzhao@live.ca>
2014-09-13 22:34:55 -0400

    resume project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lzhaoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lzhaoApp
+ */
+angular.module('lzhaoApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lzhaoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lzhaoApp
+ */
+angular.module('lzhaoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
